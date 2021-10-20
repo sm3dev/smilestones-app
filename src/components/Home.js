885d06fcf364d3562b45def2fa07901e78f0cbe7
Michael Wright <info@saymoremultimedia.com>
2021-10-19 23:20:23 -0500

    updated navbar, applicationsviews, home.js; got nav items to display; add logo image files for navbar; installed MUI/material

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Home = () => (
+    <>
+    <h2>Smilestones App</h2>
+    <p>This app allows parents and kids to create and track milestones for kids. Smilestones App, Frontend Capstone Project, C51, Nashville Software School</p>
+    </>
+)
