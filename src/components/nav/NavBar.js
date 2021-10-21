95be1c3e510d7984fcea33c48e1f94055209f642
Michael Wright <info@saymoremultimedia.com>
2021-10-21 09:53:12 -0500

    NavBar item: updated the Users link in navbar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -16,7 +16,7 @@ export const NavBar = () => {
                     <Link className="navbar__link" to="/milestone-achievements">Milestone Achievements</Link>
                 </li>
                 <li className="navbar__item">
-                    <Link className="navbar__link" to="/users">New User</Link>
+                    <Link className="navbar__link" to="/users">Users</Link>
                 </li>
                 <li className="navbar__item">
                     <Link className="navbar__link" to="/login">Login</Link>
