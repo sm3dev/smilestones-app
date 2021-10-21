19cac1c96e362783e5d730a6e6101e96f5515ad0
Michael Wright <info@saymoremultimedia.com>
2021-10-20 22:09:16 -0500

    feat/UserMilestones: updated formatting to Card

--- src/components/milestones/MilestoneResultCard.js
+++ src/components/milestones/MilestoneResultCard.js
@@ -3,30 +3,37 @@ import React from "react";
 export const MilestoneResultCard = ({ milestoneResult, milestone, user }) => {
   return (
     <>
-        <h3>{user.firstName}'s Milestone</h3>
+      <h3>{user.firstName}'s Milestone</h3>
       <h4>Milestone Type</h4>
       <h3>{milestone.name}</h3>
       <p>{milestone.description}</p>
       {milestoneResult.timeToComplete ? (
+        <>
           <div>Result: {milestoneResult.timeToComplete}</div>
-        ) : milestoneResult.length ? (
+          <div>Achieved on {milestoneResult.date}</div>
+        </>
+      ) : milestoneResult.length ? (
+        <>
           <div>Result: {milestoneResult.length}</div>
-        ) : milestoneResult.quantity ? (
+          <div>Achieved on {milestoneResult.date}</div>
+        </>
+      ) : milestoneResult.quantity ? (
+        <>
           <div>Result: {milestoneResult.quantity}</div>
-        ) : (
-          <div>Result Date: {milestoneResult.date}</div>
-        )
-      }
+          <div>Achieved on {milestoneResult.date}</div>
+        </>
+      ) : (
+        <div>Result Date: {milestoneResult.date}</div>
+      )}
 
-      <div>Primary Date field {milestoneResult.date}</div>
       <button>Details/More</button>
+      <button>Edit</button>
       <p>Remarks: {milestoneResult.remarks}</p>
       {milestoneResult.validated ? (
-            <div>Validated: Yes</div>
+        <div>Validated: Yes</div>
       ) : (
         <div>Validation Needed </div>
       )}
-      <button>Edit</button>
     </>
   );
 };
