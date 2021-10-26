74d55f8966a9a518c28cc8463ddf40e64402436c
Michael Wright <info@saymoremultimedia.com>
2021-10-26 01:00:32 -0500

    feat-Login-Register-Authentication: Login and Registration views work; Added the Milestone List to Home.js, so the home page has the available milestones listed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { MilestoneList } from './milestones/MilestoneList'
 
 export const Home = () => (
     <>
     <h2>Smilestones App</h2>
     <p>This app allows parents and kids to create and track milestones for kids. Smilestones App, Frontend Capstone Project, C51, Nashville Software School</p>
+    <hr />
+    <MilestoneList />
     </>
 )
